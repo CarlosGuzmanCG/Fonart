3cdf2083e9a8573c310e3cae1f19f2df53a79063
CarlosGuzmanCG <66038506+CarlosGuzmanCG@users.noreply.github.com>
2022-05-15 20:30:40 -0500

    act

--- page/long_rec/cart_prod_sample.php
+++ page/long_rec/cart_prod_sample.php
@@ -14,7 +14,9 @@ if(isset($_POST['add_cart_prod'])){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+
   <meta charset="UTF-8">
   
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -27,16 +29,16 @@ if(isset($_POST['add_cart_prod'])){
   <title>Producto</title>
 
 </head>
+
 <body>
 
 <?php
 include 'head.php';
 ?>
 
+  <div class="grid-container-prod">
 
-  <div class="grid-container-ft">
-
-    <div class="grit-imen-ft item1-ft img-container-prod-ft product-small-img-prod-ft">
+    <div class="grit-imen-prod item1-prod img-container-prod product-small-img-prod">
     <?php
     $produc_cart=$_SESSION['prod_bill'];
     $select_products_sc= mysqli_query($conn,"SELECT * FROM `producto` WHERE id_prod='$produc_cart'") or die ("busqueda de productos fallada");
@@ -44,10 +46,10 @@ include 'head.php';
     
     if (mysqli_num_rows($select_products_sc)>0) {
       $total = mysqli_num_rows($select_products_sc);
-      while ($fetch_products_see = mysqli_fetch_assoc($select_products_sc)) {
+      while ($fetch_products = mysqli_fetch_assoc($select_products_sc)) {
 
     ?>
-        <img  id="imageBox" src="data:image/jpeg;<?php echo $fetch_products_see['tipo_img_prod']; ?>;base64,<?php echo  base64_encode($fetch_products_see['imagen_prod']); ?>">
+        <img  id="imageBox" src="data:image/jpeg;<?php echo $fetch_products['tipo_img_prod']; ?>;base64,<?php echo  base64_encode($fetch_products['imagen_prod']); ?>">
       <?php
       };
     };
@@ -55,26 +57,72 @@ include 'head.php';
     
       </div>
 
-    <div class="grit-imen-ft item2-ft">falta</div>
-
-    <div class="grit-imen-ft item3-ft product-small-img-prod-ft">
-    <?php
-    
-    $produc_cart_2=$_SESSION['prod_bill'];
-    $select_products_sc_2= mysqli_query($conn,"SELECT * FROM `producto` WHERE id_prod='$produc_cart_2'") or die ("busqueda de productos fallada");
-
-    
-    if (mysqli_num_rows($select_products_sc_2)>0) {
-      
-      while ($fetch_products_see_2 = mysqli_fetch_assoc($select_products_sc_2)) {
-
-    ?>
-        <img  id="imageBox" src="data:image/jpeg;<?php echo $fetch_products_see_2['tipo_img_prod']; ?>;base64,<?php echo  base64_encode($fetch_products_see_2['imagen_prod']); ?>">
-
-        <img  id="imageBox" src="data:image/jpeg;<?php echo $fetch_products_see_2['tipo_img_prod']; ?>;base64,<?php echo  base64_encode($fetch_products_see_2['imagen_prod']); ?>">
-
-        <img  id="imageBox" src="data:image/jpeg;<?php echo $fetch_products_see_2['tipo_img_prod']; ?>;base64,<?php echo  base64_encode($fetch_products_see_2['imagen_prod']); ?>">
+    <div class="grit-imen-prod item2-prod product-cart-prod" >
       <?php
+    $produc_cart=$_SESSION['prod_bill'];
+    $select_products_sc= mysqli_query($conn,"SELECT * FROM `producto` WHERE id_prod='$produc_cart'") or die ("busqueda de productos fallada");
+    
+    if (mysqli_num_rows($select_products_sc)>0) {
+      $total = mysqli_num_rows($select_products_sc);
+      while ($fetch_products = mysqli_fetch_assoc($select_products_sc)) {
+
+        ?>
+
+        <div class="name-prod-cart"><p><?php echo $fetch_products['nombre_prod'];?></p></div>
+        <div class="price-prod-cart"> <?php echo "$" . $fetch_products['precio_prod'];?></div>
+        <div class="description">Descripción:</div>
+        <div class="desc_prod-cart" text-decoration:none;><?php echo $fetch_products['descripcion_prod'];?></div>
+
+        <form action="" method="post" class="box-prod">
+          <input type="hidden" name="id_prod" value="<?= $fetch_products['id_prod']; ?>">
+          <div class="produc-cart-prod-nm">
+            <input class="prod_qft" type="number" min="1" name="produc_quantify" value="1">
+          </div>
+          <div class="btn-cart-prod">
+            <input type="submit" class="btn-prod" name="add_cart_prod" value="Agregar al carrito">
+          </div>
+        </form>
+
+        <?php
+
+      };
+    };
+        ?>
+
+    </div>
+
+    <div class="grit-imen-prod item3-prod product-small-img-prod">
+    <?php
+    $produc_cart=$_SESSION['prod_bill'];
+    $select_products_sc= mysqli_query($conn,"SELECT * FROM `producto` WHERE id_prod='$produc_cart'") or die ("busqueda de productos fallada");
+    
+    if (mysqli_num_rows($select_products_sc)>0) {
+      $total = mysqli_num_rows($select_products_sc);
+      while ($fetch_products = mysqli_fetch_assoc($select_products_sc)) {
+
+        if($fetch_products['imagen_prod2'] != null){
+          ?>
+          <img  src="data:image/jpeg;<?php echo $fetch_products['tipo_img_prod']; ?>;base64,<?php echo  base64_encode($fetch_products['imagen_prod2']); ?>" onclick="myFunction(this)">
+          <?php
+        }
+    ?>
+    <?php
+        if ($fetch_products['imagen_prod3'] != null) {
+          ?>
+          <img  src="data:image/jpeg;<?php echo $fetch_products['tipo_img_prod']; ?>;base64,<?php echo  base64_encode($fetch_products['imagen_prod3']); ?>" onclick="myFunction(this)">
+          <?php
+        }
+    ?>
+
+    <?php
+    ?>
+        
+      <?php
+      if ($fetch_products['imagen_prod'] != null) {
+          ?>
+          <img  src="data:image/jpeg;<?php echo $fetch_products['tipo_img_prod']; ?>;base64,<?php echo  base64_encode($fetch_products['imagen_prod']); ?>" onclick="myFunction(this)">
+          <?php
+        }
       };
     };
 ?>
@@ -82,10 +130,14 @@ include 'head.php';
 
   </div>
 
+  <script type="text/javascript">
+    function myFunction(smallImg) {
+      var fullImg = document.getElementById("imageBox");
+      fullImg.src = smallImg.src;
+    }
+  </script>
 
-
-
-<script src="../../js/script.js"></script>
+  <script src="../../js/script.js"></script>
 <br><br><br>
 
 <?php
@@ -93,4 +145,6 @@ include 'head.php';
 ?>
 
 </body>
+
+
 </html>
